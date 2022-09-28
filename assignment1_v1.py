72741790e1950e3f7c8b99356ef75936a9a65657
Giuseppe <Giuseppe@MacBook-Pro-di-Utente.local>
2022-09-28 18:43:23 +0200

    Version 1

---
+++ assignment1_v1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# Copyright (C) 2022 Giuseppe Antonio Motisi
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import string
+import numpy as np
+import matplotlib.pyplot as plt
+
+def search_letters(strg, alphabet, number_letters):
+    alphabet=list(string.ascii_uppercase)
+    ustrg=strg.upper()
+    counter=0
+    letters_counter=np.array([])
+    while counter<number_letters:
+        letters_counter=np.insert(letters_counter,counter,int(ustrg.count(alphabet[counter])))
+        counter+=1
+    return letters_counter
+
+strg=input('Inserisci la stringa: ')
+alphabet=list(string.ascii_uppercase)
+number_letters=len(alphabet)
+counter=0
+counter_letters=search_letters(strg, alphabet, number_letters)
+while counter<number_letters:
+    print(alphabet[counter],counter_letters[counter])
+    counter+=1
+
+fig, ax = plt.subplots()
+
+ax.bar(alphabet, counter_letters)
+ax.set_ylabel('Counter Letters')
+ax.set_title('Statistics of a Book')
+
+plt.show()
