ce6d27c8145737d1867a8d13f6f72e35d193fbfb
Giuseppe <Giuseppe@MacBook-Pro-di-Utente.local>
2022-09-29 08:45:56 +0200

    collaborators

--- assignment1.py
+++ assignment1.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (C) 2022 Giuseppe Antonio Motisi
+# Copyright (C) 2022 Giuseppe Antonio Motisi, Giulia Nigrelli
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
